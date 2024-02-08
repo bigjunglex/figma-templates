7b40252669e85520364b4adac43a813617b4e7f4
bigjunglex <gearl00taz@gmail.com>
2024-02-08 21:12:20 +0300

    password validation

--- Odin-Project-Signup-Form/script.js
+++ Odin-Project-Signup-Form/script.js
@@ -2,3 +2,23 @@
 //     document.getElementById('creation').submit()
 // })
 // connecting create account button to a form for submit
+
+
+document.getElementById('submit').addEventListener('click', function(){
+    checkPassword();    
+})
+
+function checkPassword() {
+    let password = document.getElementById('password');
+    let confirmPassowrd = document.getElementById('confirm-password');
+    if (password.value.length != 0){
+        if (password.value === confirmPassowrd.value){
+            password.classList.replace('error', 'correct');
+            confirmPassowrd.classList.replace('error', 'correct');
+        } else {
+            password.classList.replace('correct', 'error');
+            confirmPassowrd.classList.replace('correct', 'error');
+        }
+    }
+}
+
